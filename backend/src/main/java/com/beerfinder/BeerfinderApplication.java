5ec86a557ee2f00222a24430493f04529d73269e
Wsakowska <72432367+Wsakowska@users.noreply.github.com>
2025-11-04 20:09:36 +0100

    setting up backend using spring initializr

---
+++ backend/src/main/java/com/beerfinder/BeerfinderApplication.java
@@ -0,0 +1,13 @@
+package com.beerfinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeerfinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeerfinderApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/beerfinder/BeerfinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beerfinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeerfinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
